ca6e9204e08f1c7d3e335e415f791943ba770a1e
JJayaL <79056716+JJayaL@users.noreply.github.com>
2022-07-04 23:48:50 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Shelf</h1>
+      <div class="headnav"><a href="index.html">Home</a>
+      <a href="https://jjayal.github.io/Halo/">Halo</a></div>
+      
+    </header>
+  );
+}
+
+export default Header;
